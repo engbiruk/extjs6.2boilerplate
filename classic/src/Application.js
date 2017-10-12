d61c1a310e5f47a9111553897e5227d3f81591bb
Biruk Tilahun <engbiruk@gmail.com>
2017-10-12 13:00:08 +0300

    Initial Commit

---
+++ classic/src/Application.js
@@ -0,0 +1,45 @@
+Ext.define('Admin.Application', {
+    extend: 'Ext.app.Application',
+    
+    name: 'Admin',
+
+    stores: [
+        'NavigationTree'
+    ],
+
+    defaultToken : 'dashboard',
+
+    // The name of the initial view to create. This class will gain a "viewport" plugin
+    // if it does not extend Ext.Viewport.
+    //
+    mainView: 'Admin.view.main.Main',
+
+    onAppUpdate: function () {
+        Ext.Msg.confirm('Application Update', 'This application has an update, reload?',
+            function (choice) {
+                if (choice === 'yes') {
+                    window.location.reload();
+                }
+            }
+        );
+    },
+
+    launch: function () {
+
+        // It's important to note that this type of application could use
+        // any type of storage, i.e., Cookies, LocalStorage, etc.
+        var loggedIn;
+
+        // Check to see the current value of the localStorage key
+        loggedIn = localStorage.getItem("AdminLoggedIn");
+
+        // This ternary operator determines the value of the TutorialLoggedIn key.
+        // If TutorialLoggedIn isn't true, we display the login window,
+        // otherwise, we display the main view
+        Ext.create({
+            xtype: loggedIn ? 'app-main' : 'login'
+            // xtype: loggedIn ? 'mainView' : 'login'
+        });
+
+    },
+});
